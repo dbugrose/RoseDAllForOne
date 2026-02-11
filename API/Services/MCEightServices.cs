e39d83ae3210b809f4d33eb0a2ece3c669bad08a
dbugrose <darcy.ophelia.rose@gmail.com>
2026-02-11 12:10:30 -0800

    more 8 Ball changes

--- API/Services/MCEightServices.cs
+++ API/Services/MCEightServices.cs
@@ -8,16 +8,10 @@ namespace API.Services
     public class MCEightServices
     {public string MagicEightBall()
         { string [] randomAnswer = ["Try again.", "Perhaps.", "Sure, why not?", "Not likely.", "Signs point to yes.", "My reply is no.", "Outlook is bright."];
-           if (question.Contains("?"))
-           { Random random = new Random();
+           Random random = new Random();
             int rndNumber = random.Next(1, 9);
             return randomAnswer[rndNumber];
-           }
 
-           else
-            {
-                return "Please enter a valid question.";
-            }
         }
     }
 }
--- API/obj/Release/net9.0/API.AssemblyInfo.cs
+++ API/obj/Release/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c9883b8afbc1638992b2c8e767e55d13f2de28c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e102199a18a2e93b533acbb9dd8e7f9a0c008f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
